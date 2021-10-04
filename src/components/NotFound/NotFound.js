434b2282f49b7306f2bc198669f426bb3f4519db
PROHOR <ahprohor88@gmail.com>
2021-10-05 03:52:49 +0600

    not found file added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="notFound-style">
             <h1>Page Note Found</h1>
             <h4>Local host : 404</h4>
         </div>
